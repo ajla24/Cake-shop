91769c6565099405e213261e29d8438dbcd499c5
ajla24 <asakic1@etf.unsa.ba>
2017-01-15 10:20:46 +0100

    Update kupovina.php

--- kupovina.php
+++ kupovina.php
@@ -11,7 +11,8 @@
 
 session_start();
 try{
-$veza = new PDO("mysql:dbname=wt8;host=localhost;charset=utf8", "wt8user", "wt8pass");
+	$veza = new PDO("mysql:dbname=baza;host=mysql-57-centos7", "korisnik", "sifra");
+
      $veza->exec("set names utf8");
 
 
