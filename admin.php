d6df8824c5615a34073a5553a5756eddf19acebd
ajla24 <asakic1@etf.unsa.ba>
2017-01-15 10:27:55 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -33,7 +33,7 @@
 		</ul>	</div>
 		<h2> <?php
 					session_start();
-$veza = new PDO("mysql:dbname=baza;host=mysql-57-centos7", "korisnik", "sifra");
+$veza = new PDO("mysql:dbname=baza;host=mysql-55-centos7", "korisnik", "sifra");
 
      $veza->exec("set names utf8");
 	
