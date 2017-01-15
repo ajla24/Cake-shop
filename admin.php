b1af51fdddb540eb26f637b132a3ad1355c966b3
ajla24 <asakic1@etf.unsa.ba>
2017-01-15 10:17:27 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -33,8 +33,8 @@
 		</ul>	</div>
 		<h2> <?php
 					session_start();
+$veza = new PDO("mysql:dbname=baza;host=mysql-57-centos7", "korisnik", "sifra");
 
-$veza = new PDO("mysql:dbname=wt8;host=localhost;charset=utf8", "wt8user", "wt8pass");
      $veza->exec("set names utf8");
 	
 if(isset($_SESSION['username']) && $_SESSION['username']=='admin'){
